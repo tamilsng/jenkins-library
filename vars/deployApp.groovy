d9a3a4b49818c81100112cf4bd4be45062aa8a0f
tamilsng <48792190+tamilsng@users.noreply.github.com>
2020-04-17 19:31:29 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
 def call(project, app, tag){
-    sh "oc new-app --name=${app} -n ${project} --image-stream=${project}/${app}:${tag} || echo 'Aplication already Exists'"
-    sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
+    sh "oc new-app --name=${app} -n ${project} --image-stream=${project}/${app}:${tag}
+    sh "oc expose service ${app} -n ${project}
 }

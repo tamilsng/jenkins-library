69a4753eb69db988a10d16f73440ea8933bbc686
tamilsng <48792190+tamilsng@users.noreply.github.com>
2020-04-24 18:36:40 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,6 +2,5 @@
 
 def call(project, app, tag){
     sh "oc new-app --name=${app} -n ${project} -l app=${app},hystrix.enabled=true --image-stream=${project}/${app}:${tag} || echo 'Aplication already Exists'"
-    sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
     sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/api/health"
 }
